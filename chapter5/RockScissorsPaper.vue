39e0da5b982df165e82dbc61f559009f0571d705
L-Hojun <65013690+L-Hojun@users.noreply.github.com>
2020-05-13 18:50:09 +0900

    2020/05/13
    second commit

--- chapter5/RockScissorsPaper.vue
+++ chapter5/RockScissorsPaper.vue
@@ -2,8 +2,8 @@
     <div>
         <div id="computer" v-bind:style="computedStyleObject"></div>
         <template>
-            <button @click="onClickButton('가위')">가위</button>
             <button @click="onClickButton('바위')">바위</button>
+            <button @click="onClickButton('가위')">가위</button>
             <button @click="onClickButton('보')">보</button>
         </template>
         <div>{{result}}</div>
@@ -17,7 +17,20 @@
         가위: '-142px',
         보: '-284px',
     };
+
+    const scores = {
+        가위: 1,
+        바위: 0,
+        보: -1,
+    };
+
+    const computerChoice = (imgCoord) => {
+        return Object.entries(rspCoords).find(function(v){
+            return v[1] === imgCoord;
+        })[0];
+    };
     
+    let interval = null;
     export default {
         data() {
             return{
@@ -29,14 +42,61 @@
         computed: {
             computedStyleObject() {
                 return {
-                    background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg)`,
+                    background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`,
                 };
             }
         },
         methods: {
-            onClickButton(choice) {
-
+            changeHand() {
+                interval = setInterval(() => {
+                    if(this.imgCoord === rspCoords.바위) {
+                        this.imgCoord = rspCoords.가위;
+                    } else if (this.imgCoord === rspCoords.가위) {
+                        this.imgCoord = rspCoords.보;
+                    } else if (this.imgCoord === rspCoords.보) {
+                        this.imgCoord = rspCoords.바위;
+                    }
+                }, 100);
             },
+
+            onClickButton(choice) {
+                clearInterval(interval);
+                const myScore = scores[choice];
+                const cpuScore = scores[computerChoice(this.imgCoord)];
+                const diff = myScore - cpuScore;
+                if (diff === 0) {
+                    console.log(diff);
+                    this.result = '비겼습니다.';
+                } else if ([-1,2].includes(diff)){
+                    console.log(diff);
+                    this.result = '이겼습니다.';
+                    this.score++;
+                } else {
+                    console.log(diff);
+                    this.result = '졌습니다.';
+                    this.score--;
+                }
+                setTimeout(() => {
+                    this.changeHand();
+                }, 1000);
+            },
+        },
+        created() {
+            console.log('created');
+        },
+        mounted() {
+            console.log('mounted');
+            this.changeHand();
+        },
+        updated() {
+            console.log('updated');
+        },
+        beforeDestroy() {
+            console.log('beforeDestroy');
+            clearInterval(interval);
+        },
+        destroyed() {
+            console.log('destroyed');
         },
     };
 </script>
